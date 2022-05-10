16e241bf606e98e8e0168edeeb973543fe6052c6
Jesus Elizalde <elizaldejesus34@gmail.com>
2022-05-10 10:51:02 -0700

    bug

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,3 +2,5 @@ from .db import db
 from .user import User
 from .business import Business
 from .customer import Customer
+from .category import Category
+from .service import Service
---
+++ app/models/category.py
@@ -0,0 +1,22 @@
+from .db import db
+from datetime import datetime
+
+class Category(db.Model):
+    __tablename__ = 'categories'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+    image = db.Column(db.String(255))
+    business_id = db.Column(db.Integer,db.ForeignKey("businesses.id"), nullable=False)
+
+    business = db.relationship("Business",back_populates="category")
+
+    service = db.relationship("Service",back_populates="category")
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name':self.name,
+            'image':self.image,
+            'business_id':self.business_id,
+        }
