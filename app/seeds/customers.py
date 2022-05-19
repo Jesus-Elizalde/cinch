638a680ec7cad00a71dd8eb3b1f67e23182e0386
Jesus Elizalde <elizaldejesus34@gmail.com>
2022-05-19 09:13:13 -0700

    seed data

--- app/seeds/customers.py
+++ app/seeds/customers.py
@@ -10,7 +10,24 @@ def seed_customers():
 
     for customer_dict in data:
 
-        new_customer = Customer(first_name=customer_dict["firstName"],display_name="1",email="1",last_name=customer_dict["lastName"],street=customer_dict["adress"],city=customer_dict["city"],state=customer_dict["state"],country="USA",postal_code=customer_dict["zipCode"],mobile_number=customer_dict["price"],home_number=customer_dict["homeNumber"],work_number=customer_dict["homeNumber"],business_id=1)
+        new_customer = Customer(
+            first_name=customer_dict["firstName"],
+            last_name=customer_dict["lastName"],
+            display_name=customer_dict["firstName"],
+            street=customer_dict["address"]["address"],
+            city=customer_dict["address"]['city'],
+            state=customer_dict["address"]["state"],
+            country="USA",
+            postal_code=customer_dict["address"]["postalCode"],
+            lat=customer_dict["address"]['coordinates']["lat"],
+            long=customer_dict["address"]['coordinates']["lng"],
+            mobile_number=customer_dict["phone"],
+            home_number=customer_dict["phone"],
+            email=customer_dict["email"],
+            company=customer_dict["company"]["name"],
+            job_title=customer_dict["company"]["title"],
+            work_number=customer_dict["phone"],
+            business_id=1)
         db.session.add(new_customer)
 
     db.session.commit()
