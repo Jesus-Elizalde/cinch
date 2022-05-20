83b16a86a9d908b10b1e3e23732a68d0f9605a8a
Jesus Elizalde <elizaldejesus34@gmail.com>
2022-05-20 10:44:19 -0700

    modal

--- app/forms/customers_form.py
+++ app/forms/customers_form.py
@@ -3,11 +3,11 @@ from wtforms import StringField , IntegerField
 from wtforms.validators import DataRequired
 
 class CustomerForm(FlaskForm):
-    first_name=StringField("first_name")
-    last_name=StringField("last_name")
-    display_name=StringField("display_name")
+    first_name=StringField("first_name",validators=[DataRequired()])
+    last_name=StringField("last_name",validators=[DataRequired()])
+    display_name=StringField("display_name",validators=[DataRequired()])
     street=StringField("street",validators=[DataRequired()])
-    # street=StringField("street",validators=[DataRequired()])
+
     city=StringField("city",validators=[DataRequired()])
     state=StringField("state",validators=[DataRequired()])
     country=StringField("country",validators=[DataRequired()])
@@ -16,7 +16,7 @@ class CustomerForm(FlaskForm):
     long=StringField("long")
     mobile_number=StringField("mobile_number")
     home_number=StringField("home_number")
-    email=StringField("email")
+    email=StringField("email",validators=[DataRequired()])
     company=StringField("company")
     job_title=StringField("job_title")
     work_number=StringField("work_number")
