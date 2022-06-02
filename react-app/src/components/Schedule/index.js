5505fbeba46f00e38ff6be4ae4692faab21c8707
Jesus Elizalde <elizaldejesus34@gmail.com>
2022-06-01 17:03:16 -0700

    delele and edited working

--- react-app/src/components/Schedule/index.js
+++ react-app/src/components/Schedule/index.js
@@ -21,68 +21,62 @@ import {
   ConfirmationDialog,
 } from "@devexpress/dx-react-scheduler-material-ui";
 import { useDispatch, useSelector } from "react-redux";
-import { getJobsDetailts } from "../../store/job";
-
-// import Paper from "@mui/material/Paper";
-// import { ViewState, EditingState } from "@devexpress/dx-react-scheduler";
-// import {
-//   Scheduler,
-//   Toolbar,
-//   TodayButton,
-//   DateNavigator,
-//   ViewSwitcher,
-//   WeekView,
-//   MonthView,
-//   DayView,
-//   Appointments,
-//   EditRecurrenceMenu,
-//   AppointmentForm,
-//   ConfirmationDialog,
-// } from "@devexpress/dx-react-scheduler-material-ui";
+import { deleteJobDetails, editJobDetails } from "../../store/job";
 
 const Schedule = () => {
   const dispatch = useDispatch();
 
   const rawJobs = useSelector((state) => Object.values(state.jobs));
+  console.log("🚀 ~ file: index.js ~ line 30 ~ Schedule ~ rawJobs", rawJobs);
 
   const jobs = rawJobs?.map((job) => {
     return {
-      startDate: job.from_date_time,
-      endDate: job.to_date_time,
-      title: job.customer_id,
+      startDate: new Date(job.from_date_time),
+      endDate: new Date(job.to_date_time),
+      title: `${job.customer_id}`,
+      id: job.id,
+      notes: job.message,
+      customer_id: job.customer_id,
+      job_ids: job.services.map((service) => service.id).join("-"),
     };
   });
 
-  const [data, setData] = useState(jobs);
-  console.log("🚀 ~ file: index.js ~ line 54 ~ Schedule ~ data", data);
+  const [data, setData] = useState([]);
 
   const [currentViewName, setCurrentViewName] = useState("work-week");
   const [selectedDate, setSelectedDate] = useState(new Date());
 
-  useEffect(() => {
-    dispatch(getJobsDetailts());
-  }, [dispatch]);
-
   const commitChanges = ({ added, changed, deleted }) => {
     if (added) {
-      const startingAddedId =
-        data.length > 0 ? data[data.length - 1].id + 1 : 0;
-
-      setData([...data, { id: startingAddedId, ...added }]);
+      // const startingAddedId =
+      //   data.length > 0 ? data[data.length - 1].id + 1 : 0;
+      // setData([...data, { id: startingAddedId, ...added }]);
+      console.log("WNP");
     }
 
     if (changed) {
-      setData(
-        data.map((appointment) =>
-          changed[appointment.id]
-            ? { ...appointment, ...changed[appointment.id] }
-            : appointment
-        )
-      );
+      const jobId = +Object.keys(changed)[0];
 
-      if (deleted !== undefined) {
-        setData(data.filter((appointment) => appointment.id !== deleted));
-      }
+      const editedJob = {
+        ...jobs?.filter((appointment) => +appointment.id === jobId)[0],
+        ...changed[jobId],
+      };
+
+      dispatch(
+        editJobDetails({
+          id: editedJob.id,
+          from_date_time: editedJob.startDate,
+          to_date_time: editedJob.endDate,
+          message: editedJob.notes,
+          customer_id: "1",
+          job_ids: editedJob.job_ids,
+        })
+      );
+    }
+
+    if (deleted !== undefined) {
+      //   setData(data.filter((appointment) => appointment.id !== deleted));
+      dispatch(deleteJobDetails(deleted));
     }
     return { data };
   };
@@ -97,7 +91,7 @@ const Schedule = () => {
 
   return (
     <Paper>
-      <Scheduler data={data} height={750}>
+      <Scheduler data={jobs} height={750}>
         <ViewState
           defaultCurrentDate={selectedDate}
           currentViewName={currentViewName}
@@ -127,61 +121,6 @@ const Schedule = () => {
       </Scheduler>
     </Paper>
   );
-
-  //   const [data, setData] = useState([
-  //     {
-  //       startDate: "2018-11-01T09:45",
-  //       endDate: "2018-11-01T11:00",
-  //       title: "Meeting",
-  //     },
-  //   ]);
-  //   const [currentViewName, setCurrentViewName] = useState("work-week");
-  //   const [selectedDate, setSelectedDate] = useState(new Date());
-  //   const [title, setTitle] = useState("");
-  //   const [startDate, setStartDate] = useState("");
-  //   const [endDate, setEndDate] = useState("");
-  //   const currentViewNameChange = (currentViewName) => {
-  //     setCurrentViewName(currentViewName);
-  //   };
-  //   const selectedDateChange = (currentDate) => {
-  //     setSelectedDate({ currentDate });
-  //   };
-  //   const appointmentDataChange = ({ title, endDate, startDate }) => {
-  //     setTitle(title);
-  //     setEndDate(endDate);
-  //     setStartDate(startDate);
-  //   };
-  //   return (
-  //     <Paper>
-  //       <Scheduler data={data} height={660}>
-  //         <ViewState
-  //           defaultCurrentDate={selectedDate}
-  //           currentViewName={currentViewName}
-  //           onCurrentViewNameChange={currentViewNameChange}
-  //           onCurrentDateChange={selectedDateChange}
-  //         />
-  //         <WeekView startDayHour={10} endDayHour={19} />
-  //         <WeekView
-  //           name="work-week"
-  //           displayName="Work Week"
-  //           excludedDays={[0, 7]}
-  //           startDayHour={7}
-  //           endDayHour={19}
-  //         />
-  //         <MonthView />
-  //         <DayView />
-  //         <Toolbar />
-  //         <DateNavigator />
-  //         <TodayButton />
-  //         <ViewSwitcher />
-  //         <Appointments />
-  //         <AppointmentForm
-  //           onAppointmentDataChange={appointmentDataChange}
-  //           appointmentData={{ title, startDate, endDate }}
-  //         />
-  //       </Scheduler>
-  //     </Paper>
-  //   );
 };
 
 export default Schedule;
